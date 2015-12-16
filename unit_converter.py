1c8283af30aef4feb455e383544ec60a8319d2ca
swingmineur <maciej_mackiewicz@yahoo.com>
2015-12-16 22:26:07 +0100

    Added new units

--- unit_converter.py
+++ unit_converter.py
@@ -27,14 +27,34 @@ class Unit(object):
         self.unit_dictionary = unit_dictionary
 
 
+# LENGTH
+
 inches = Unit(0.39370079, {"in": "cm", "inch": "centimeter", "inches": "centimeters"})
 feet = Unit(3.28, {"feet": "meters", "ft": "m", "foot": "meters"})
-sq_feet = Unit(10.7639, {'square feet': 'square meters'})
 yards = Unit(1.09361, {"yard": "meters", "yards": "meters", "yd": "m"})
 miles = Unit(1.609344, {"miles": "kilometers", "mi": "km"})
+
+# TEMPERATURE
 temperature = Unit(9, {"°F": "°C", "degrees Fahrenheit": "degrees Celsius", "degrees F": "degrees C", "F": "C"})
 
-# To Do - add new units
+# MASS
+ounces = Unit(0.035274, {"ounces": "grams", "ounce": "grams", "oz": "g"})
+pounds = Unit(2.20462, {"pounds": "kilograms", "pound": "kilogram", "lb": "kg"})
+stones = Unit(0.157473, {"stones": "kilograms", "stone": "kilogram", "st": "kg"})
+
+# AREA
+
+sq_inch = Unit(0.157473, {"square inches": "square centimeters",
+                          "square inch": "square centimeters", "sq in": "sq cm", "in²": "cm²"})
+sq_feet = Unit(10.7639, {"square feet": "square meters", "sq ft": "sq m", "square foot": "square meters", "ft²": "m²"})
+sq_yards = Unit(1.19599, {"square yards": "square meters",
+                          "square yards": "square meters", "sq yd": "sq m", "yd²": "m²"})
+sq_miles = Unit(0.386102, {"square miles": "square kilometers",
+                           "square mile": "square kilometers", "sq mi": "sq km", "mi²": "km²"})
+# VOLUME
+pints = Unit(1.75975, {"pints": "litres", "pint": "litres", "pt": "l"})
+quarts = Unit(0.879877, {"quarts": "litres", "quart": "litres", "gt": "l"})
+gallons = Unit(0.219969, {"gallons": "litres", "gallon": "litres", "gal": "l"})
 
 
 def convert_text(text, unit, i):
